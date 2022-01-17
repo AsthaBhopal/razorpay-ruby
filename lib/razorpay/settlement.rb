13e4d925b339b894d289b23a26e4860677906b24
shauryam <shauryam@asthatrade.com>
2022-01-17 18:49:48 +0530

    bug fix

--- lib/razorpay/settlement.rb
+++ lib/razorpay/settlement.rb
@@ -2,21 +2,24 @@ require 'razorpay/request'
 require 'razorpay/entity'
 
 module Razorpay
-	class Settlement < Entity
-		def self.request
-	      Razorpay::Request.new('settlements')
-	    end
+  # Card API allows you to fetch cards
+  # saved with Razorpay
+  # Docs: https://docs.razorpay.com/v1/page/cards
+  class Settlement < Entity
+    def self.request
+        Razorpay::Request.new('settlements')
+    end
 
-	    def self.fetch(id)
-	      request.fetch id
-	    end
+    def self.fetch(id)
+      request.fetch id
+    end
 
-	    def self.all(options = {})
-	      request.all options
-	    end
+    def self.all(options = {})
+      request.all options
+    end
 
-	    def self.recon(options = {})
-	    	request.get 'recon/combined', options
-	    end 
-	end 
-end 
+    def self.recon(options = {})
+      request.get 'recon/combined', options
+    end 
+  end
+end
